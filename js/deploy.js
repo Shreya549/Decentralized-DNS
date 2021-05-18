70219b555846b29ce7286b6ba004f2a755897f84
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2021-05-19 00:28:24 +0530

    feat: deployer :construction_worker:

--- js/deploy.js
+++ js/deploy.js
@@ -4,7 +4,7 @@ const Tx = require('ethereumjs-tx').Transaction
 const Web3 = require("web3");
 const web3 = new Web3(process.env.TESTNET_URL);
 const secretKey1 = Buffer.from(process.env.SECRET_KEY.substring(2, 66), "hex");
-const account1 = "0x71D9641D53B0020C3f1dd0776C6E978c247DaD86";
+const account1 = "0x2b135743063e963CFDE6c4A701d20D4054E0baB9"; //deployed account
 
 const contractAddress = process.env.CONTRACT_ADDRESS;
 
@@ -12,14 +12,14 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
 
     //Smart contract data
     const data = require('./data.js').data;
-    console.log(txCount)
+    
     //Create Transaction Object
     const txObject = {
       from: account1,
-      to: contractAddress,
+      //   to: contractAddress,
       nonce: web3.utils.toHex(txCount),
-      gasLimit: web3.utils.toHex(1000000),
-      gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
+      gasLimit: web3.utils.toHex(6000000),
+      gasPrice: web3.utils.toHex(web3.utils.toWei("100", "gwei")),
       data: data,
     };
     
@@ -32,7 +32,11 @@ web3.eth.getTransactionCount(account1, (err, txCount) => {
 
     //Broadcast the transaction
     web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-        console.log('err:', err , "txHash:", txHash)
+        console.log('err:', err, "txHash:", txHash)
+        web3.eth.getTransactionReceipt(txHash, (error, receipt) => {
+          console.log("err:", error);
+          console.log("Receipt:", receipt);
+        });
     })
 })
 
